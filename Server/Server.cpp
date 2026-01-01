9ec55cf2558916b7c01eade68449913745664453
Han97 <hcw9711@gmail.com>
2026-01-01 13:05:49 +0900

    [Server] 솔루션에 Server 프로젝트 추가

---
+++ Server/Server.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+
+#define PORT_NUM 7777
+
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    std::wcout.imbue(std::locale(""));
+
+    //shared_ptr<TcpServerService> serverService = std::make_shared<TcpServerService>(
+    //    NetAddress(L"0.0.0.0", PORT_NUM),
+    //    []() -> shared_ptr<Session> { return make_shared<ClientSession>(); },
+    //    5);
+
+    //ASSERT_CRASH(serverService->StartServer());
+
+    //for (int i = 0; i < 5; ++i)
+    //{
+    //    GThreadManager->Launch([&serverService]() {
+    //        serverService->GetIocpCore().WorkerThreadLoop();
+    //        });
+    //}
+
+    GThreadManager->Join();
+}
