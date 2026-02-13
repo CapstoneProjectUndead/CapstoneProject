aa3d41d4e49f1c3cf5a3ca86738b088dec6101bb
Han97 <hcw9711@gmail.com>
2026-02-13 19:32:18 +0900

    [Client] CPlayer 상대캐릭터 보간 코드 수정

--- Client/Player.cpp
+++ Client/Player.cpp
@@ -128,11 +128,26 @@ void CPlayer::OpponentMoveSyncByInterpolation(float elapsedTime)
             state = PLAYER_STATE::IDLE;
         }
         else {
-            // 상태 동기화
-            state = frameB->state;
+            // [보간 이동]
+            // Frame A와 Frame B 사이의 실제 이동 거리 계산
+            // (서버가 보내준 두 점 사이의 거리가 얼마나 되는가?)
+            float intervalDist = Vector3::Length(Vector3::Subtract(frameB->position, frameA->position));
 
-            // 위치 동기화
-            // 미세하게 움직이든 멈춰있든, 보간된 좌표로 매 프레임 이동
+            // 두 패킷 사이의 거리가 1cm 미만(0.01f)이면 '사실상 멈춤'으로 간주
+            if (intervalDist < 0.01f) {
+
+                state = PLAYER_STATE::IDLE;
+
+                if (frameA->state == PLAYER_STATE::WALK)
+                    state = frameA->state;
+                if (frameB->state == PLAYER_STATE::WALK)
+                    state = frameB->state;
+            }
+            else {
+                state = PLAYER_STATE::WALK;
+            }
+
+            // 위치 이동
             SetPosition(nextPos);
         }
     }
