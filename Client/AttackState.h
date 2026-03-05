91fa5d744ea8631b417aa1ad86c71e0456728e87
Han97 <hcw9711@gmail.com>
2026-03-05 15:46:52 +0900

    [Client] AttackState 추가

---
+++ Client/AttackState.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "State.h"
+
+class CAttackState :
+    public CState
+{
+public:
+    CAttackState();
+    ~CAttackState();
+
+    virtual void Update(float elapsedTime) override;
+    virtual void Enter() override;
+    virtual void Exit() override;
+};
+
