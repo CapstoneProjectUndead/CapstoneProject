5f95498faa59d308eaa8bffbca301c7dee7d4f60
Han97 <hcw9711@gmail.com>
2026-04-19 15:54:25 +0900

    [Server] CMineableObject 클래스 추가

---
+++ Server/MineableObject.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+// ServerÂÊ MineableObject
+#include "MineableObject.h"
+
+CMineableObject::CMineableObject()
+	: CObject(OBJECT_TYPE::MINEABLE_OBJECT)
+{
+}
+
+CMineableObject::~CMineableObject()
+{
+}
+
+void CMineableObject::Initialize()
+{
+	CObject::Initialize();
+
+	hp = max_hp;
+}
+
+void CMineableObject::Update(const float dt)
+{
+	CObject::Update(dt);
+}
+
+void CMineableObject::TakeDamage()
+{
+	if (hp <= 0) return;
+	--hp;
+}
---
+++ Server/MineableObject.h
@@ -0,0 +1,22 @@
+#pragma once
+// ServerÂÊ MineableObject
+#include "Object.h"
+
+class CMineableObject : public CObject
+{
+public:
+    CMineableObject();
+    ~CMineableObject();
+
+    virtual void Initialize() override;
+    virtual void Update(const float dt) override;
+
+public:
+    void TakeDamage();
+    bool IsDestroyed() const { return hp <= 0; }
+    int GetHp() const { return hp; }
+
+private:
+    int hp = 5;
+    const int max_hp = 5;
+};
