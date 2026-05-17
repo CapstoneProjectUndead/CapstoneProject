6eadf5883eb2d0e711acc52e6d2dddd2028ead0c
Han97 <hcw9711@gmail.com>
2026-05-17 16:52:55 +0900

    [Server] LoadGameScene 함수에서 obj_treasure.bin 파일 읽기

--- Server/GameScene.cpp
+++ Server/GameScene.cpp
@@ -222,6 +222,15 @@ void CGameScene::LoadGameScene()
 			LoadFrameNode(prototypes, children);
 		}
 	}
+	{
+		std::string fileName{ "../Modeling/obj_treasure.bin" };
+		auto frameRoot = CGeometryLoader::LoadGeometry(fileName);
+
+		LoadFrameNode(prototypes, frameRoot);
+		for (const auto& children : frameRoot->childrens) {
+			LoadFrameNode(prototypes, children);
+		}
+	}
 }
 
 void CGameScene::CreateGameScene()
